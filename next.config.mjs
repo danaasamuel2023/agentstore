5d8003bf53e1766860c9e32561e4b4474268c97e
danaasamuel2023 <danaasamuel20frimpong@gmail.com>
2025-09-09 22:18:36 -1200

    syntax

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-img-element': 'off',
+      'react-hooks/exhaustive-deps': 'warn'
+    }
+  }
+};
 
 export default nextConfig;
